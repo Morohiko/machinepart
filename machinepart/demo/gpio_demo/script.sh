d7827bc7f2191a878231a22377487c4acbd7c5d2
Morohiko <qkatashi@gmail.com>
2020-03-24 19:12:02 +0000

    MP: add examples for using gpio

---
+++ machinepart/demo/gpio_demo/script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+
+sleep 1
+echo 1 > /sys/class/gpio/gpio17/value
+sleep .0015
+echo 0 > /sys/class/gpio/gpio17/value
+
+sleep 1
+echo 1 > /sys/class/gpio/gpio17/value
+sleep .0015
+echo 0 > /sys/class/gpio/gpio17/value
+
+sleep 1
+echo 1 > /sys/class/gpio/gpio17/value
+sleep .0015
+echo 0 > /sys/class/gpio/gpio17/value
+
+sleep 1
+echo 1 > /sys/class/gpio/gpio17/value
+sleep .0015
+echo 0 > /sys/class/gpio/gpio17/value
+
+sleep 1
+echo 1 > /sys/class/gpio/gpio17/value
+sleep .0015
+#echo 0 > /sys/class/gpio/gpio12/value
